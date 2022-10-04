048cbb78e21377a372ac65f56be88fc2f5a28b29
Mikhail Zayarnykh <mishabest@mail.ru>
2022-10-04 16:13:45 +0700

    lesson_5 upload

---
+++ 5/1methodConsoleApp3/1methodConsoleApp3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1methodConsoleApp3
+{
+    class Program
+    {
+        // Метод вывода текста (Enter для продолжения выполнения кода)
+        static void Print(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+        // Метод задержки
+        static void Delay()
+        {
+            Console.ReadKey(true);
+        }
+        // Метод вывода массива построчно
+        static string[] PrintInLines(string[] Args)
+        {
+            string[] t = new string[Args.Length];
+            for (int i = 0; i < Args.Length; i++)
+            {
+                t[i] = Args[i];
+                Console.WriteLine(Args[i]);
+            }
+            return t;
+        }
+        // Основной код
+        static void Main(string[] args)
+        {
+            Print("Задание 1. Метод разделения строки на слова");
+            Print("Введите предложение:");
+            string entrString = Console.ReadLine();
+            string[] sentenceString = entrString.Split(' ');
+            Console.ForegroundColor = ConsoleColor.Green;
+            Print("Вывод целиком");
+            Console.ResetColor();
+            Print(entrString);
+            Delay();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Print("Вывод построчно");
+            Console.ResetColor();
+            PrintInLines(sentenceString);
+            Delay();
+        }
+
+
+}
+}
---
+++ 5/2MethodConsoleApp1/2MethodConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2MethodConsoleApp3
+{
+    class Program
+    {
+        // Метод вывода текста (Enter для продолжения выполнения кода)
+        static void Print(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+
+        // Метод конвертрования строки в массив и реверсированный вывод
+        static void ToArrayAndInvert(string Text)
+        {
+            string[] TextToArray = Text.Split(' ');
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Обычный текст");
+            Console.ResetColor();
+            Console.WriteLine($"{Text}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Реверсированный массив");
+            Console.ResetColor();
+            Array.Reverse(TextToArray);
+            for (int i = 0; i < TextToArray.Length; i++)
+            {
+                Console.Write($"{TextToArray[i]} ");
+            }
+        }
+
+        // Метод задержки
+        static void Delay()
+        {
+            Console.ReadKey(true);
+        }
+
+        // Основной код
+        static void Main(string[] args)
+        {
+            Print("Задание 2. Перестановка слов в предложении");
+            Print("Введите предложение:");
+            string entrString = Console.ReadLine();
+            ToArrayAndInvert(entrString);
+            Delay();
+        }
+
+
+    }
+}
