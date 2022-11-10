7958f6465e551977b3358069faa0d6d3c8cd7a84
Mikhail Zayarnykh <mishabest@mail.ru>
2022-11-10 12:28:46 +0700

    homework 8 update

--- 8/4/8_4_ConsoleApp1/8_4_ConsoleApp1/Program.cs
+++ 8/4/8_4_ConsoleApp1/8_4_ConsoleApp1/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 using System.Xml.Linq;
 
 namespace _8_4_ConsoleApp1
